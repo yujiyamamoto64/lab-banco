e2ffe3a5bf1e06be2b0d16dc18c135921435f759
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2026-02-25 19:12:01 -0300

    Update HelloController.java

--- app/src/main/java/com/lab/banco/HelloController.java
+++ app/src/main/java/com/lab/banco/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    
     @GetMapping("/")
     public String hello() {
         return "Lab Banco v1";
